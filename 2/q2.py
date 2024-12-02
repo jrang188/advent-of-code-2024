2f2d4883d53db2c3c168a3fc89fe14780bbbe700
Justin Ang <jrang@sfu.ca>
2024-12-02 02:15:44 -0800

    finished q2 day 2

--- 2/q2.py
+++ 2/q2.py
@@ -1,41 +1,41 @@
-def tooRapid(level1, level2):
+def tooRapidOrNothing(level1, level2):
     diff = abs(level1 - level2)
-    return diff > 3
-
-def noChange(level1, level2):
-    diff = abs(level1 - level2)
-    return diff < 1
+    return diff < 1 or diff > 3
 
 
 def isAscending(levels):
-    incidentCounter = 0
     for i in range(1, len(levels)):
-        if levels[i] < levels[i - 1] or noChange(levels[i], levels[i - 1]):
-            incidentCounter += 1
-        if tooRapid(levels[i], levels[i - 1]):
+        if levels[i] < levels[i - 1] or tooRapidOrNothing(levels[i], levels[i - 1]):
             return False
-    return incidentCounter < 2
+    return True
 
 
 def isDescending(levels):
-    incidentCounter = 0
     for i in range(1, len(levels)):
-        if levels[i] > levels[i - 1] or noChange(levels[i], levels[i - 1]):
-            incidentCounter += 1
-        if tooRapid(levels[i], levels[i - 1]):
+        if levels[i] > levels[i - 1] or tooRapidOrNothing(levels[i], levels[i - 1]):
             return False
-    return incidentCounter < 2
+    return True
+
+
+def isSafe(levels):
+    return isAscending(levels) or isDescending(levels)
+
+
+def isTolerable(levels):
+    for i in range(len(levels)):
+        if isSafe(levels[:i] + levels[i + 1 :]):
+            return True
+    return False
 
 
 safeCounter = 0
-with open("input_test.txt") as f:
+with open("input.txt") as f:
     reports = f.readlines()
     for report in reports:
         # split by space and cast to int
         levels = [int(level) for level in report.split()]
 
-        if isAscending(levels) or isDescending(levels):
-            print(levels)
+        if isSafe(levels) or isTolerable(levels):
             safeCounter += 1
 
 print(safeCounter)
