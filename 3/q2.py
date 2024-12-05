3b9c06397e47a1c29d798557b68abf6c85487433
Justin Ang <jrang@sfu.ca>
2024-12-05 00:31:23 -0800

    finished q2 day 3. This is regex hell

--- 3/q2.py
+++ 3/q2.py
@@ -1,18 +1,28 @@
 import re
+import itertools
 
-with open("input_test.txt") as f:
+with open("input.txt") as f:
     res = 0
-    mulDict = {}
+    
     mulPattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    doPattern = r"do\(\)"
+    dontPattern = r"don't\(\)"
+    toggle = True
     for line in f:
-        mulMatches = re.finditer(mulPattern, line)
-
-        for mulMatch in mulMatches:
-            num1, num2 = map(int, mulMatch.groups()) 
-            i = mulMatch.start()
-            dict[(num1, num2)] = i
-            
-    print(dict)
-
+        orderedMatches = []
+        matches = itertools.chain(re.finditer(mulPattern, line), re.finditer(doPattern, line), re.finditer(dontPattern, line))        
+        for match in matches:
+            orderedMatches.append((match.start(), match))
+        orderedMatches.sort(key=lambda x: x[0])
+        
+        for match in orderedMatches:
+            if match[1].group(0) == "do()":
+                toggle = True
+            elif match[1].group(0) == "don't()":
+                toggle = False
+            else:
+                num1, num2 = int(match[1].group(1)), int(match[1].group(2))
+                if toggle:
+                    res += num1 * num2
+                    
     print(res)
-            
